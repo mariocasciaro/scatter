3333ecfa89f4ed2567de3130328de953f73e0865
Klaus Sevensleeper <k7sleeper@gmail.com>
2013-12-13 14:52:33 +0100

    Feature Request: directory as dependency (#12)

--- test/01-load.js
+++ test/01-load.js
@@ -1,4 +1,3 @@
-
 var expect = require('chai').expect,
   fs = require('fs'),
   rimraf = require('rimraf'),
@@ -358,4 +357,31 @@ describe('Scatter basic loading', function() {
       }).otherwise(done);
     });
   });
+
+  describe("directory dependencies (#12)", function () {
+    var scatter;
+    before(function () {
+      scatter = new Scatter();
+      scatter.registerParticles(TEST_DIR + '/dirdep');
+    });
+
+    it('should load and return a module with directory dependency', function (done) {
+      scatter.load('Module1').then(function (mod) {
+        expect(mod).to.exist;
+        expect(mod).to.have.property('prop', 'module1');
+        expect(mod).to.have.property('depProp', 'dep');
+        done();
+      }).otherwise(done);
+    });
+
+    it('should prefer files over directories', function (done) {
+      scatter.load('dep2').then(function (mod) {
+        expect(mod).to.exist;
+        expect(mod).to.have.property('prop', 'dep2');
+        done();
+      }).otherwise(done);
+    });
+
+  });
+
 });
---
+++ test/01-load/dirdep/dep2/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  prop: "index@dep2"
+};
